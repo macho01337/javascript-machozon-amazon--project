41deb4f8e3f47d341891d043f134842d94420aaf
machon01337 <gluhakmartin@gmail.com>
2024-10-12 17:14:43 +0200

    Regenerate HTML when updating delivery option

--- data/cart.js
+++ data/cart.js
@@ -7,11 +7,12 @@ if(!cart){
         {
         productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity: 2,
-        
+        deliveryOptionId:'2'
     },
     {
         productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
         quantity: 1, 
+        deliveryOptionId:'2'
     }
     ];
 }
@@ -38,6 +39,7 @@ export function addToCart(productId){
   cart.push({
    productId: productId,
    quantity:  1,
+   deliveryOptionId: '1'
    });
   }
 
@@ -56,5 +58,18 @@ newCart.push(cartItem);
 
 
 cart = newCart;
+saveToStorage();
+}
+
+export function updateDeliveryOption( productId , deliveryOptionId){
+    let matchingItem;
+
+    
+    cart.forEach((cartItem)=>{
+   if (productId === cartItem.productId) {
+  matchingItem = cartItem;
+     }
+  }); 
+  matchingItem.deliveryOptionId = deliveryOptionId;
 saveToStorage();
 }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,8 @@
 import{cart,addToCart}from'../data/cart.js';
 import{products}from'../data/products.js';
 import { formatCurrency } from './utils/money.js';
- let productsHTML = '';
+ 
+let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `   
@@ -66,7 +67,7 @@ function updateCartQuantity(){
    cartQuantity += cartItem.quantity;
   });
     
-  document.querySelectorAll('.js-cart-quantity')
+  document.querySelector('.js-cart-quantity')
   .innerHTML = cartQuantity;
   
 }
